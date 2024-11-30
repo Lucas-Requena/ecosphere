cfb465f0af35fe78438d6f68f7870fb01ca1110f
Lucas-Requena <lucas.requena.foch@gmail.com>
2024-11-30 20:23:31 +0100

    edit mémoire valeur fini

--- ecosphere/app.py
+++ ecosphere/app.py
@@ -145,9 +145,10 @@ def valid_edit_evaluation():
     tuple_sql = (Nom_Animateur, DateSeance, Nom_Participant, Note_Seance, Note_Animation, id_evaluation)
     mycursor.execute(sql, tuple_sql)
     get_db().commit()
-    message = u'evaluation modifié , animateur :' + Nom_Animateur, "date :" + DateSeance, "participant :" + Nom_Participant, "note de seance :" + Note_Seance, "note d'animation" + Note_Animation
+    message = u'evaluation ajouté , animateur :'+Nom_Animateur,"date :"+DateSeance,"participant :"+Nom_Participant,"note de seance :"+Note_Seance,"note d'animation"+Note_Animation
     flash(message, 'alert-success')
     return redirect('/evaluation/show')
+
 
 
 ###Seance###
